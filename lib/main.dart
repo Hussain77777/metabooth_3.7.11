67c1e868ba1343793031ee3df4baff9df84b8d5e
Hussain77777 <80383610+Hussain77777@users.noreply.github.com>
2023-09-18 15:48:32 +0500

    changes added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:metabooth/screens/add_device_screen.dart';
 import 'package:metabooth/screens/color.dart';
 import 'package:metabooth/screens/color_picker_screen.dart';
+import 'package:metabooth/screens/splash_screen.dart';
 import 'package:metabooth/screens/welocme_screen.dart';
 import 'package:metabooth/screens/login_or_register_screen.dart';
 import 'package:metabooth/screens/login_screen.dart';
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WelcomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:metabooth/constants/size_config.dart';
+import 'package:metabooth/screens/welocme_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool bigLogo = false;
+  late Timer _timer;
+
+  Future navigateToWelcome() async {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        bigLogo = true;
+      });
+    });
+    Future.delayed(Duration(seconds: 2), () {
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>WelcomeScreen()));
+    });
+
+  }
+void dispose(){
+    _timer.cancel();
+    super.dispose();
+}
+  void initState() {
+    navigateToWelcome();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: SizeConfig.width(context, 0.05),
+          ),
+          alignment: Alignment.center,
+          height: SizeConfig.height(context, 1),
+          width: SizeConfig.width(context, 1),
+          //color: Colors.yellow,
+          child: (bigLogo == true)
+              ? Image.asset("assets/logo_2.png",
+                  width: SizeConfig.width(context, 1))
+              : Image.asset(
+                  "assets/logo.png",
+                  width: SizeConfig.width(context, 0.3),
+                ),
+        ),
+      ),
+    );
+  }
+}
